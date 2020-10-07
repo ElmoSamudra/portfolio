cb189a44a9b85a4cbcf3f3ba46f70446824c29a9
Elmo <Shawnsamudra09@users.noreply.github.com>
2020-10-07 22:38:48 +1100

    change footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
 
 const contactItems = [
   {
@@ -18,27 +19,26 @@ const contactItems = [
 ];
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexShrink: "0",
+    margin: "auto",
+    padding: "5% 5% 1% 5%",
   },
 }));
 
 export default function Footer() {
   const classes = useStyles();
   return (
-    <footer className={classes.root}>
-      <Typography variant="h6" align="center" gutterBottom>
-        Shawn E Samudra
-      </Typography>
-      <Typography variant="h6" align="center" gutterBottom>
+    <Grid className={classes.root}>
+      <Typography variant="h6">Shawn E Samudra</Typography>
+      <Typography variant="h6">
         <EmailIcon />
         Shawnsamudra09@gmail.com
       </Typography>
       <Link href="https://www.linkedin.com/in/shawn-samudra-734524150/">
-        <Typography variant="h6" align="center" gutterBottom>
+        <Typography variant="h6">
           <LinkedInIcon />
           Shawn Samudra
         </Typography>
       </Link>
-    </footer>
+    </Grid>
   );
 }
